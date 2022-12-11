a569a4efc636fefeef5d90e4896d74054015f406
tsaialex12 <112999798+tsaialex12@users.noreply.github.com>
2022-12-11 22:45:03 +0800

    Create Class14.java

---
+++ Class14.java
@@ -0,0 +1,11 @@
+package test;
+
+public class hw01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String a ="Habit is second nature";
+		System.out.print(a.substring(16));
+	}
+
+}
