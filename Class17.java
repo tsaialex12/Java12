ab7eb3277f169c6c4e1b552665b13f18ff739daa
tsaialex12 <112999798+tsaialex12@users.noreply.github.com>
2022-12-11 23:28:42 +0800

    Create Class17.java

---
+++ Class17.java
@@ -0,0 +1,13 @@
+package test;
+
+public class hw01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String a = "262904713";
+		Short b = Short.parseShort(a);
+		System.out.print(b);
+	}
+
+}
+// 數值超出範圍
