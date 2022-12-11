9646cb5fc5576112013551415d2ed66c8c0266f1
tsaialex12 <112999798+tsaialex12@users.noreply.github.com>
2022-12-11 23:23:40 +0800

    Create Clas16.java

---
+++ Clas16.java
@@ -0,0 +1,12 @@
+package test;
+
+public class hw01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String a = "262904713";
+		int b = Integer.parseInt(a);
+		System.out.print(b*2);
+	}
+
+}
