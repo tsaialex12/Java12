9b0011477a9f265c9c1e587a23c339ede2f99a7e
tsaialex12 <112999798+tsaialex12@users.noreply.github.com>
2022-12-11 23:01:33 +0800

    Create Class15.java

---
+++ Class15.java
@@ -0,0 +1,13 @@
+package test;
+
+public class hw01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String a ="Habit is second nature";
+		double b = 123.45;
+		String c = Double.toString(b);
+		System.out.print(c.length());
+	}
+
+}
